f5fed43b2467059e386c46b87dd1d2fc0909a6e4
vineet <vineet@digitzeco.in>
2022-01-30 10:45:08 +0530

    added cursor animation

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,6 +7,7 @@ import ExperienceComponent from './components/experience';
 import TopCategoriesComponent from './components/topCategories';
 import HireMeComponent from './components/hireMe';
 import './style.scss';
+import AnimatedCursor from "react-animated-cursor"
 
 
 class HomeContainer extends Component {
@@ -27,6 +28,13 @@ class HomeContainer extends Component {
         console.log('rerendering')
         return (
             <div className={"hor-row home-component-main" + ( this.state.currentMode === 'dark' ? '' : ' light-mode-container')}>
+                <AnimatedCursor
+                    innerSize={12}
+                    outerSize={12}
+                    color='11, 168, 193'
+                    outerAlpha={0.2}
+                    innerScale={0.7}
+                    outerScale={5}/>
                 <LandingComponent
                     scrollTo = {() => this.scrollTo() } />
                 <NavbarComponent
